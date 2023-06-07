3f9e339b7b338bf04cc4eed99a650f0f05f717ec
matias GODdoy <mgodoy@bitsion.com>
2023-06-07 11:17:05 -0300

    Versión con el botón funcionando y valor inicial

--- projects/libreria/src/lib/libreria.component.ts
+++ projects/libreria/src/lib/libreria.component.ts
@@ -1,19 +1,38 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges, OnInit } from '@angular/core';
 import { IButton } from '../interfaces/IButton';
 
 @Component({
   selector: 'lib-libreria',
   template: `
-    <button class="btn btn-primary" [type]="myButton.type"]>{{ myButton.text}}</button>
+    <button class="btn btn-primary" [type]="button.type">
+      {{ button.text }}
+    </button>
   `,
-  styles: [
-  ]
+  styles: [],
 })
-
-export class LibreriaComponent {
+export class LibreriaComponent implements OnInit, OnChanges {
   @Input() myButton: IButton = {
     text: 'Ejemplo',
-    type: 'primary'
+    type: 'primary',
+  };
+
+  button: IButton = {
+    text: 'Ejemplo',
+    type: 'primary',
+  };
+
+  constructor() {}
+
+  ngOnInit(): void {
+    if (this.myButton) {
+      this.button = { ...this.button, ...this.myButton };
+    }
   }
 
+  ngOnChanges(): void {
+    if (this.myButton) {
+      this.button = { ...this.button, ...this.myButton };
+    }
+    console.log('ngOnChanges', this.button);
+  }
 }
